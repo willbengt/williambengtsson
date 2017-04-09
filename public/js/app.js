e5629c2eeee4933db18f68e27140c1ba02a2a6b7
willbengt <wilbe075@student.liu.se>
2017-04-09 16:50:23 +0200

    Fixed style

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,4 @@
 var wbApp = angular.module("wbApp", ["xeditable"]);
-wbApp.controller('FirstController', function($scope) {   
-  $scope.first = 'Some';
-  $scope.last = 'One';
-  $scope.heading = 'Message: ';
-  $scope.updateMessage = function() {
-    $scope.message = 'Hello ' + $scope.first +' '+ $scope.last + '!';
-  };
-});
-
-wbApp.controller('FormCtrl', function($scope) {
-  $scope.user = {
-    name: 'awesome user'
-  };  
-});
 
 wbApp.controller('EditableRowCtrl', function($scope, $filter, $http) {
   $scope.users = [
