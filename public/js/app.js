a2f6282cc5113c4cdd1e500725a0670d53c81097
willbengt <wilbe075@student.liu.se>
2017-04-09 13:00:28 +0200

    Added editable form

--- public/js/app.js
+++ public/js/app.js
@@ -14,6 +14,30 @@ wbApp.controller('FormCtrl', function($scope) {
   };  
 });
 
+wbApp.controller('EditableRowCtrl', function($scope, $filter, $http) {
+  $scope.users = [
+    {id: 1, name: 'awesome user1'},
+    {id: 2, name: 'awesome user2'},
+    {id: 3, name: 'awesome user3'}
+  ]; 
+
+  $scope.saveUser = function(data, id) {
+    angular.extend(data, {id: id});
+  };
+
+  $scope.removeUser = function(index) {
+    $scope.users.splice(index, 1);
+  };
+
+  $scope.addUser = function() {
+    $scope.inserted = {
+      id: $scope.users.length+1,
+      name: '',
+    };
+    $scope.users.push($scope.inserted);
+  };
+});
+
 wbApp.run(function(editableOptions) {
   editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
 });
